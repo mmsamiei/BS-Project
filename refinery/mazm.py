44a3c94f4e5031f33731e958d277f2caa0fda4ce
mmsamiei <mohmahsamiei@gmail.com>
2018-07-27 09:23:46 +0430

    finish phase two

--- refinery/mazm.py
+++ refinery/mazm.py
@@ -5,3 +5,20 @@ class Mazm:
         normalizer = hazm.Normalizer()
         result = normalizer.normalize(string)
         return result
+    def my_word_tokenizer(string):
+        result = hazm.word_tokenize(string)
+        return result
+    def my_lemmatizer(string):
+        stemmer = hazm.Stemmer()
+        lemmatizer = hazm.Lemmatizer()
+        current = lemmatizer.lemmatize(string)
+        # new = stemmer.stem(current)
+        # while(current != new):
+        #     current = new
+        #     new = stemmer.stem(current)
+        # result = new
+        if("#" in current): # return bone mozareh!!
+            result = current.split("#")[1]
+        else:
+            result = current
+        return result
--- refinery/refinery.py
+++ refinery/refinery.py
@@ -7,21 +7,42 @@ class Refinery:
     mongo_port = 27017
     mongo_database = 'scraping'
     collections_name = ['test']
+    destination_collection_name = 'refined'
+    
     def __init__(self):
         self.client = MongoClient(Refinery.mongo_server, Refinery.mongo_port)
         self.db = self.client[Refinery.mongo_database]
         self.string = "ss"
+    
     def start(self):
         for collection_name in Refinery.collections_name:
             collection = self.db[str(collection_name)]
             self.refine_collection(collection)
+    
     def refine_collection(self, collection):
         for post in collection.find({"checked": False}):
-            self.refine_post(post)
-    def refine_post(self, post):
-        new_title = mazm.my_normalizer(post['title'])
-        new_body = mazm.my_normalizer(post['body'])
-        print(new_title)
+            self.refine_post(post, collection)
+    
+    def refine_post(self, post, source_collection):
+        new_title = self.refine_text(mazm.my_normalizer(post['title']))
+        new_body = self.refine_text(mazm.my_normalizer(post['body']))
+        url = post['url']
+        refined_post = {"title":new_title, "body": new_body, "url":url, "source": source_collection.name, "checked": False}
+        self.insert_post_destination(refined_post)
+
+    def insert_post_destination(self, post):
+        refined_posts = self.db[self.destination_collection_name]
+        post_id = refined_posts.insert_one(post).inserted_id
+        return post_id
+
+    def refine_text(Self, text):
+        new_text = mazm.my_normalizer(text)
+        new_text_words = mazm.my_word_tokenizer(new_text)
+        text_list = []
+        for word in new_text_words:
+            text_list.append(mazm.my_lemmatizer(word))
+        return (" ".join(text_list))
+                
 if __name__ == "__main__":
     refinery = Refinery()
     refinery.start()
