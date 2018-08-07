be9f9a4cef0fb22c8dbafa6c0460fe4a03fd62ec
mmsamiei <mohmahsamiei@gmail.com>
2018-08-07 12:57:47 +0430

    in the middle ov periodic

--- crawlers/crawler_center/MyPeriodic.py
+++ crawlers/crawler_center/MyPeriodic.py
@@ -1,13 +1,14 @@
 import time, threading
 import os
 import datetime
-i = 0 
 
-class MyThread():
 
-    def __init__(self, foo2):
-        self.interval = 1
+class MyPeriodic():
+
+    def __init__(self, foo2, interval):
+        self.interval = interval 
         self.foo2 = foo2
+        self.i = 0
     
     def start(self):
         self.t = threading.Timer(1, self.foo)
@@ -16,11 +17,11 @@ class MyThread():
     def foo(self):
         global i
         print(datetime.datetime.now())
-        i = i + 1
+        self.i = self.i + 1
+        self.foo2()
         self.t = threading.Timer(self.interval, self.foo)
         self.t.start()
     
-
     def change_interval(self, new_interval):
         self.interval = new_interval
 
@@ -29,7 +30,9 @@ def my_foo():
     print("1")
 
 if __name__ == "__main__":
-    my_thread = MyThread(my_foo)
+    my_thread = MyPeriodic(my_foo, 1)
     my_thread.start()
+    time.sleep(6)
+    my_thread.change_interval(2)
         
     
