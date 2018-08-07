4725a76fc7e8524d42afedaa43aa997513a73921
mmsamiei <mohmahsamiei@gmail.com>
2018-08-07 13:45:08 +0430

    before ninisite extends Periodic

--- crawlers/crawler_center/ninisite.py
+++ crawlers/crawler_center/ninisite.py
@@ -1,10 +1,14 @@
 import time, threading
 import os
+from MyPeriodic import MyPeriodic
+
 def foo():
     os.system("scrapy crawl shortTerm")
     print("finish crawl ninisite")
-    t = threading.Timer(1, foo)
-    t.start()
 
-os.chdir("../ninisite/ninisite/")
-foo()
+if __name__ == "__main__":
+    os.chdir("../ninisite/ninisite/")
+    my_thread = MyPeriodic(foo, 60)
+    my_thread.start()
+    time.sleep(240)
+    my_thread.change_interval(120)
